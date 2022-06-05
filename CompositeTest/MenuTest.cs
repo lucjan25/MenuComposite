03d4c48e2130b5dd5a7b8dfd8e490f47951b310d
lucjan25 <lucjan25.t@gmail.com>
2022-06-05 20:24:37 +0200

    Add project files.

---
+++ CompositeTest/MenuTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using MenuBiblioteka;
+
+namespace CompositeTest
+{
+    internal class MenuTest
+    {
+        static void Main(string[] args)
+        {
+            Menu glowne = new("glowne", "http://glowne.menu/menu");
+            Menu podmenu = new("podmenu", "http://glowne.menu/menu/pod");
+            MenuItem strona = new("strona", "http://glowne.menu/menu/strona");
+            MenuItem podstrona = new("podstrona", "http://glowne.menu/menu/pod/podstrona");
+            MenuItem drugapodstrona = new("drugapodstrona", "http://glowne.menu/menu/pod/druga");
+
+            glowne.DisplayMenu();
+            Console.WriteLine("---");
+
+            glowne.Add(strona);
+            glowne.Add(podmenu);
+            strona.Add(podstrona);
+            podmenu.Add(podstrona);
+            podmenu.Add(drugapodstrona);
+            glowne.DisplayMenu();
+            Console.WriteLine("---");
+        }
+    }
+}
---
+++ MenuBiblioteka/MenuComposite.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MenuBiblioteka
+{
+    public abstract class MenuComponent
+    {
+        protected string url;
+        protected string name;
+        public MenuComponent(string name, string url)
+        {
+            this.name = name;
+            this.url = url;
+        }
+        public string GetName()
+        {
+            return name;
+        }
+        public string GetUrl()
+        {
+            return url;
+        }
+        public abstract void Add(MenuComponent component);
+        public abstract void DisplayMenu();
+    }
+    public class Menu : MenuComponent
+    {
+        List<MenuComponent> subMenus = new List<MenuComponent>();
+        public Menu(string name, string url)
+            : base(name, url)
+        {
+        }
+        public override void Add(MenuComponent component)
+        {
+            subMenus.Add(component);
+        }
+        public override void DisplayMenu()
+        {
+            Console.WriteLine(GetName() + " " + GetUrl());
+            foreach (MenuComponent component in subMenus)
+            {
+                component.DisplayMenu();
+            }
+        }
+    }
+    public class MenuItem : MenuComponent
+    {
+        public MenuItem(string name, string url)
+            : base(name, url)
+        {
+        }
+        public override void Add(MenuComponent component)
+        {
+            return;
+        }
+        public override void DisplayMenu()
+        {
+            Console.WriteLine(GetName() + " " + GetUrl());
+        }
+    }
+}
